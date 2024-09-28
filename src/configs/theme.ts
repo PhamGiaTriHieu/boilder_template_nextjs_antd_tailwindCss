6e051535a7063df269e4c54373c363188cc18089
Hieu Pham <phamgiatrihieu@gmail.com>
2024-09-28 22:56:51 +0700

    init template NextJS14 with antd and tailwindCss

---
+++ src/configs/theme.ts
@@ -0,0 +1,10 @@
+import type { ThemeConfig } from "antd";
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: "#52c41a", // may be changed according to your design
+  },
+};
+
+export default theme;
